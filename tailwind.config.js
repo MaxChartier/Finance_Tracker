64c6add37a7fc0a8a7e0623960347bb01b8fad91
max chartier <chartiermini@gmail.com>
2024-09-21 15:02:13 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './html/home.html',
+    './html/connection_page.html',
+    // Ajoute le chemin vers ton fichier HTML
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
